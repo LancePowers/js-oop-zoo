f048e7673663dcec8e318a1c6be4448f13d4c07a
Lance <Lance@thelpstory.com>
2015-08-08 21:29:29 -0600

    Exercise Complete

--- spec/zooSpec.js
+++ spec/zooSpec.js
@@ -14,55 +14,68 @@ describe('Zoo', function(){
 
   describe('#changeLocation', function(){
     it('should change locations', function(){
-      // add spec
+      zoo.changeLocation('Colorado Springs')
+      expect(zoo.location).toEqual('Colorado Springs');
     });
   });
 
   describe('#open', function(){
     it('should change status to open', function(){
-      // add spec
+      zoo.open();
+      expect(zoo.openClose).toEqual(true);
     });
   });
 
 
   describe('#isOpen', function(){
     it('should see if the zoo is open', function(){
-      // add spec
+      zoo.open();
+      expect(zoo.isOpen()).toEqual(true);
     });
     it('should see if the zoo is closed', function(){
-      // add spec
+      expect(zoo.isOpen()).toEqual(false);
     });
   });
 
   describe('#animals', function(){
     it('should initially be empty', function(){
-      // add spec
+      expect(zoo.animals).toEqual([]);
     });
   });
 
 
   describe('#addAnimal', function(){
     it('should only add an animal to the animals array when the zoo is open', function(){
-      // add spec
+      expect(zoo.addAnimal(lion)).toEqual('Sorry Ray, The Fucking Zoo is Closed');
     });
     it('should add an animal to the animals array', function(){
-      // add spec
+      zoo.open();
+      zoo.addAnimal(lion);
+      expect(zoo.animals).toEqual([lion]);
     });
 
     it('should only add instances of animals', function(){
-      // add spec
+      zoo.open();
+      expect(zoo.addAnimal('car')).toEqual('That\'s not an animal.')
     });
 
     it('should not add duplicates', function(){
-      // add spec
+      zoo.open();
+      zoo.addAnimal(pig);
+      expect(zoo.addAnimal(pig)).toEqual('Already got one of them');
+      expect(zoo.animals).toEqual([pig]);
+
     });
   });
 
   describe('#removeAnimal', function(){
     it('should remove an animal from the animals array if the zoo is open', function(){
-      // add spec
+      expect(zoo.removeAnimal()).toEqual('Sorry Ray, The Fucking Zoo is Closed')
+      zoo.open()
+      zoo.addAnimal(pig);
+      zoo.addAnimal(lion);
+      zoo.removeAnimal(pig);
+      expect(zoo.animals).toEqual([lion]);
     });
   });
 });
-
-
--- src/zoo.js
+++ src/zoo.js
@@ -3,6 +3,59 @@ var Animal = require("./animal");
 function Zoo(name, location){
   this.name = name;
   this.location = location;
+  this.openClose = false;
+  this.animals = [];
 }
+
+Zoo.prototype.addAnimal = function (animal) {
+  if(this.notDup(animal) !== true){
+    return 'Already got one of them';
+  } else if (this.isOpen() !== true){
+    return 'Sorry Ray, The Fucking Zoo is Closed';
+  } else if(this.isAnimal(animal)!== true){
+    return 'That\'s not an animal.';
+  } else{
+    this.animals.push(animal);
+  }
+};
+
+Zoo.prototype.notDup = function (animal) {
+  if(this.animals.indexOf(animal) === -1){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Zoo.prototype.removeAnimal = function (animal) {
+  var index = this.animals.indexOf(animal);
+  if (this.isOpen() !== true){
+    return 'Sorry Ray, The Fucking Zoo is Closed';
+  } else {this.animals.splice(index,1)};
+};
+
+Zoo.prototype.isAnimal = function(animal){
+  if(animal instanceof Animal){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Zoo.prototype.changeLocation = function (location) {
+  this.location = location;
+};
+
+Zoo.prototype.open = function () {
+  this.openClose = true;
+};
+
+Zoo.prototype.isOpen = function () {
+  if (this.openClose === true){
+    return true;
+  } else {
+    return false;
+  }
+};
 
 module.exports = Zoo;
